3e09cbd84d1925e97e5efbd804fc9603ed40a6ba
Yair Quintana <yairquintanamx@gmail.com>
2023-03-05 04:16:20 -0600

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app(settings_module='config.development'):
+    app = Flask(__name__)
+    app.config.from_object(settings_module)
+
+    @app.route('/')
+    def index():
+        return 'Index'
+
+    return app
